53f9d4f5f4276b62e8243342c80722ba96e6873c
Eugene Popov <popoff.zhenya@yandex.ru>
2024-04-07 21:32:52 +0300

    step 1.10 DI and IO

--- src/main/java/com/popov/spring/spring_introduction/Test3.java
+++ src/main/java/com/popov/spring/spring_introduction/Test3.java
@@ -11,10 +11,9 @@ public class Test3 {
 //        Pet pet = context.getBean("myPet", Pet.class);
 //        Pet pet = new Dog();
         Person person = context.getBean("myPerson", Person.class);
-        person.callYourPet();
-
         System.out.println(person.getSurname());
         System.out.println(person.getAge());
+        person.callYourPet();
         context.close();
     }
 }
