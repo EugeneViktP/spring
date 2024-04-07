e35c54db10eff81ce62d2ff6c99008b2120a4641
Eugene Popov <popoff.zhenya@yandex.ru>
2024-04-07 22:19:18 +0300

    step 1.12 Bean Scope

---
+++ src/main/java/com/popov/spring/spring_introduction/Test4.java
@@ -0,0 +1,12 @@
+package com.popov.spring.spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test4 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext2.xml");
+
+
+    }
+}
