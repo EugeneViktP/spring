a4c9601d5d5e1872ddf34473258c7a7e2d8ed261
Eugene Popov <popoff.zhenya@yandex.ru>
2024-04-13 22:30:05 +0300

    Autowired for constructor

--- src/main/java/com/popov/spring/spring_introduction/Cat.java
+++ src/main/java/com/popov/spring/spring_introduction/Cat.java
@@ -2,7 +2,7 @@ package com.popov.spring.spring_introduction;
 
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component ("catBean")
 public class Cat implements Pet{
     public Cat() {
         System.out.println("Cat bean is created");
